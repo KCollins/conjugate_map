29c6a1f793f0024ee211ce8563d062cff6fa3f74
KCollins <kvc2@case.edu>
2024-05-22 14:32:47 -0400

    Update __init__.py

--- conjugate_map/__init__.py
+++ conjugate_map/__init__.py
@@ -3,5 +3,5 @@ from importlib import metadata
 from conjugate-map import conjCalcFunctions
 
 # Set version
-__version__ = metadata.version('conjugate-map')
+__version__ = metadata.version('conjugate_map')
 
