05793cac81c69f32847f753bb560d89bd0ef3a6b
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2024-09-23 12:31:33 -0400

    BUG: fixed docs configuration
    Added missing lines to the docs configuration and removed an unneeded line.

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
 
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
@@ -57,7 +58,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "sphinx_rtd_theme"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# -- Options for autodoc -------------------------------------------------
+autodoc_mock_imports = ["geopack", "gpxpy"]
